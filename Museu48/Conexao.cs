0c1001e47b0419dd898800204d6ca55c4c7c83b9
Victor-Emanuel-Porto <81031972+Victor-Emanuel-Porto@users.noreply.github.com>
2021-11-12 14:04:01 -0300

    Add files via upload

---
+++ Museu48/Conexao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Data;
+using MySqlConnector;
+
+
+
+
+namespace Museu48
+{
+    class Conexao
+    {
+        
+        
+    }
+}
---
+++ Museu48/Entrada.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Museu48
+{
+    public partial class Entrada : Form
+    {
+        public Entrada()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+
+            Saida a = new Saida();
+
+            a.ShowDialog();
+            this.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            
+            
+            this.Close();
+            
+            Application.Exit();
+        }
+    }
+}
